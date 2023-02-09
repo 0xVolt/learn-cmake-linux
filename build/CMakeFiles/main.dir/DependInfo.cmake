5e9796af024b091db9a02ccde9e7ef9377d8e899
Desh Iyer <desh.iyer@gmail.com>
2023-02-09 17:32:00 +0530

    Updated linux build files

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/volt/code/learn-cmake-linux/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/home/volt/code/learn-cmake-linux/source/FIR.cpp" "CMakeFiles/main.dir/source/FIR.cpp.o" "gcc" "CMakeFiles/main.dir/source/FIR.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/volt/code/learn-cmake-linux
+# Build directory: /home/volt/code/learn-cmake-linux/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
